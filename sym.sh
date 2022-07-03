71671529cdb9edb0b985b19e20a87a43fdb831b5
dk949 <56653556+dk949@users.noreply.github.com>
2022-07-04 01:45:10 +0300

    removed dependency on xdotool

--- sym.sh
+++ sym.sh
@@ -1,4 +1,3 @@
 #bin/sh
 
-printf "≤ ≥ ≠ ∞ ∧ ∨ ¬ ≡ ⌊ ⌋ ↓ ↑ ← → ↤ ↦ ⇐ ⇒ ⇔ ↔ ∃ ∀ ∴ Ø ℕ ℤ ∈ ∉ ⊆ ⊂ ⊎ ∪ ∩  □ ×" | tr " " "\n" | dmenu -c -l 6 -g 6 | tr -d '\n' | xclip -i -selection c
-xdotool key "Control_L+V"
+printf "≤ ≥ ≠ ∞ ∧ ∨ ¬ ≡ ⌊ ⌋ ↓ ↑ ← → ↤ ↦ ⇐ ⇒ ⇔ ↔ ∃ ∀ ∴ • Ø ℕ ℤ ∈ ∉ ⊆ ⊂ ⊎ ∪ ∩  □ ×" | tr " " "\n" | dmenu -c -l 6 -g 6 | tr -d '\n' | xclip -i -selection c
