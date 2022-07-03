a9d0bb88555527e138b90f30f6cd3db72b4f8a42
dk949 <56653556+dk949@users.noreply.github.com>
2022-07-04 01:45:45 +0300

    WIP, currently doen't work. needs to be terminal agnostic

--- bookmark.sh
+++ bookmark.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# XXX: Doesn't work atm
 #$(awk '{for (i = 1; i <= NF; i++) print $i}' "$XDG_CONFIG_HOME"/bookmarks)
 declare -A BOORMARKS
 
@@ -6,4 +7,4 @@ while read -r line; do
     BOORMARKS+=(["$(echo "$line" | cut -d' ' -f1)"]="$(echo "$line" | cut -d' ' -f2)")
 done < "$XDG_CONFIG_HOME"/bookmarks
 
-eval nohup alacritty --working-directory "${BOORMARKS["$(echo "${!BOORMARKS[@]}" | tr " " "\n" | dmenu -l 4 -g 5 -c)"]}" 1>/dev/null 2>/dev/null </dev/null  &
+eval nohup "sh -c cd ${BOORMARKS["$(echo "${!BOORMARKS[@]}" | tr " " "\n" | dmenu -l 4 -g 5 -c)"]}; $TERMINAL" 1>/dev/null 2>/dev/null </dev/null  &
