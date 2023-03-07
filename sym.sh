2b18c16ab193cb65670ee46074dcae4f772be07d
dk949 <56653556+dk949@users.noreply.github.com>
2023-03-07 14:59:13 +0000

    sym

--- sym.sh
+++ sym.sh
@@ -1,3 +1,4 @@
 #bin/sh
 
 printf "≤ ≥ ≠ ∞ ∧ ∨ ¬ ≡ ⌊ ⌋ ↓ ↑ ← → ↤ ↦ ⇐ ⇒ ⇔ ↔ ∃ ∀ ∴ • Ø ℕ ℤ ∈ ∉ ⊆ ⊂ ⊎ ∪ ∩  □ ×" | tr " " "\n" | dmenu -c -l 6 -g 6 | tr -d '\n' | xclip -i -selection c
+xdotool key "Control_L+V"
