6356951cbedd7ac37e3fc4bba178973cb98c3d2f
dk949 <56653556+dk949@users.noreply.github.com>
2025-05-07 23:33:54 +0100

    updated screenshot

--- screenshot.sh
+++ screenshot.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
+APPNAME=screenshot
+die() {
+    heading=$1
+    body=$2
+    # shellcheck disable=SC2086
+    notify-send -a "$APPNAME" -u critical "$heading" $body
+}
+notify() {
+    heading=$1
+    body=$2
+    # shellcheck disable=SC2086
+    notify-send -a "$APPNAME" -u low "$heading" $body
+}
 
-SCREENSHOT_DIR="$HOME/Pictures/screenshots/"
+SCREENSHOT_DIR=${SCREENSHOT_DIR:-"$HOME/Pictures/screenshots/"}
 
-[ -d "$SCREENSHOT_DIR" ] || mkdir -p "$SCREENSHOT_DIR"
+err=$(mkdir -p "$SCREENSHOT_DIR" 2>&1) \
+    || die "Could not create screenshot" "$err"
 
-FILENAME="$(printf "" | dmenu -l 0 )"
-
-if [ -z "$FILENAME" ]; then
-    FILENAME="$(date +'%F')"
-fi
-
-FILENAME="$SCREENSHOT_DIR"/"$FILENAME"
+filename="$(printf "" | dmenu -l 0 )" || exit 0
+filename=$SCREENSHOT_DIR/${filename:-$(date +'%F')}
 
 
-COUNTER=0
-while [ -f "$FILENAME"_"$COUNTER".png ] ; do
-    COUNTER=$((COUNTER+1))
-done
+ctr=0
+while real_filename="$filename"_"$ctr".png \
+    && [ -f "$real_filename" ]
+do : $((ctr+=1)); done
 
-FILENAME="$FILENAME"_"$COUNTER".png
-
-killall unclutter
-import "$FILENAME"
-unclutter -b
+killall unclutter && restore=1
+err=$(import "$real_filename" 2>&1) || die "import error" "$err"
+notify "$(basename "$real_filename")"
+[ -n "$restore" ] && unclutter -b
